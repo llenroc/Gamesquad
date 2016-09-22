e4285723c902083950d6cc7c1a577abd2806c47a
Shane Burton <burtosha13@gmail.com>
2016-09-21 22:24:34 -0700

    Wierdly only commited half the files last time

--- GameSquad/src/GameSquad/Hubs/ChatHub.cs
+++ GameSquad/src/GameSquad/Hubs/ChatHub.cs
@@ -23,7 +23,13 @@ namespace GameSquad.Hubs
 
     public static class ConnectedUsers
     {
-        public static HashSet<UserDetail> Users = new HashSet<UserDetail>();
+        // public static HashSet<UserDetail> Users = new HashSet<UserDetail>();
+
+            /// <summary>
+            /// Username is key, connection id is value
+            /// </summary>
+        public static Dictionary<string, string> Users = new Dictionary<string, string>();
+        
 
     }
 
@@ -37,26 +43,85 @@ namespace GameSquad.Hubs
         /// <param name="userName">Gets the username </param>
         public void Connect(string userName)
         {
-            var newUser = new UserDetail { ConnectionId = Context.ConnectionId, Username = userName };
+            //var newUser = new UserDetail { ConnectionId = Context.ConnectionId, Username = userName };
 
-            if(ConnectedUsers.Users.Count(u => u.ConnectionId == newUser.ConnectionId) == 0)
+
+            // if (ConnectedUsers.Users.Count(u => u.Username == newUser.Username) > 0 || userName == null)
+
+            //Used in the trygetvalue for testing
+           string keyTest;
+            if (ConnectedUsers.Users.TryGetValue(userName, out keyTest))
             {
-                Clients.Caller.onConnected(ConnectedUsers.Users);
-                ConnectedUsers.Users.Add(newUser);
+                //Tells the client side to disconnect client. Current crappy way of fixing multiple browser tab issue
+                Clients.Caller.onConnected(0);
 
+            }
+            //else if (ConnectedUsers.Users.Count(u => u.ConnectionId == newUser.ConnectionId) == 0)
+            else
+            {
+                var returnList = new List<UserDetail>();
+                foreach (var item in ConnectedUsers.Users.ToList())
+                {
+                    returnList.Add(new UserDetail
+                    {
+                        Username = item.Key,
+                        ConnectionId = item.Value
+                    });
+                }
+
+                Clients.Caller.onConnected(returnList);
+
+                // ConnectedUsers.Users.Add(newUser);
+                ConnectedUsers.Users.Add(userName, Context.ConnectionId);
+
+                var newUser = new UserDetail
+                {
+                    Username = userName,
+                    ConnectionId = Context.ConnectionId
+                };
                 Clients.AllExcept(Context.ConnectionId).onNewUserConnected(newUser);
             }
-            
+
+
+
 
         }
 
         public override Task OnDisconnected(bool stopCalled)
         {
-            var userToRemove = ConnectedUsers.Users.FirstOrDefault(u => u.ConnectionId == Context.ConnectionId);
-            if (userToRemove != null)
+            //var userToRemove = ConnectedUsers.Users.FirstOrDefault(u => u.ConnectionId == Context.ConnectionId);
+            //var test = ConnectedUsers.Users.Contains(Context.User);                           
+
+            //string userToRemove = ConnectedUsers.Users.FirstOrDefault(;
+           // ConnectedUsers.Users.TryGetValue()
+
+            if (ConnectedUsers.Users.ContainsValue(Context.ConnectionId))
             {
-                ConnectedUsers.Users.Remove(userToRemove);
-                Clients.All.onUserDisconnected(userToRemove.Username);
+
+                var userToRemove = ConnectedUsers.Users.Where(u => u.Value == Context.ConnectionId).FirstOrDefault();
+
+                ConnectedUsers.Users.Remove(userToRemove.Key);
+                Clients.All.onUserDisconnected(userToRemove.Key);
+
+
+                //if (ConnectedUsers.Users.Count(u => u.Username == userToRemove.Username) > 1)
+                //{
+                //    var usersToRemove = ConnectedUsers.Users.Where(u => u.Username == userToRemove.Username).ToList();
+
+                //    foreach (UserDetail user in usersToRemove)
+                //    {
+                //        ConnectedUsers.Users.Remove(user);
+                //        Clients.All.onUserDisconnected(userToRemove.Username);
+
+                //    }
+                //}
+
+                //else
+                //{
+                //    //var userToRemove = ConnectedUsers.Users.FirstOrDefault(u => u.ConnectionId == Context.ConnectionId);
+                //    ConnectedUsers.Users.Remove(userToRemove);
+                //    Clients.All.onUserDisconnected(userToRemove.Username);
+                //}
             }
             
             return base.OnDisconnected(stopCalled);
@@ -71,14 +136,38 @@ namespace GameSquad.Hubs
             Clients.All.newMessage(message);
         }
 
-        public void SendPrivateMessage(string fromUsername, string privateMessage, string toUserName)
+        public void SendPrivateMessage(string fromUsername, string privateMessage, string toUserName, string toConnectionId)
         {
-            var toUser = ConnectedUsers.Users.FirstOrDefault(u => u.Username == toUserName);
+            
 
-            if (toUser != null)
+            try
             {
-                Clients.Client(toUser.ConnectionId).getPrivateMessage(fromUsername, privateMessage, fromUsername);
-                Clients.Caller.getPrivateMessage(fromUsername, privateMessage, toUserName);
+                if(toUserName == "Dummy User 1" || toUserName == "Dummy User 2")
+                {
+
+                    var dummyMessage = "You sent to the server: " + privateMessage;
+
+                    Clients.Caller.getPrivateMessage(fromUsername, privateMessage, toUserName);
+
+                    Clients.Caller.getPrivateMessage(toUserName, dummyMessage, toUserName);
+
+
+
+
+                }
+
+                else
+                {
+                    Clients.Client(toConnectionId).getPrivateMessage(fromUsername, privateMessage, fromUsername);
+                    Clients.Caller.getPrivateMessage(fromUsername, privateMessage, toUserName);
+                }
+
+                
+            }
+            catch
+            {
+                var errorMsg = "Server: Unable to deliver message";
+                Clients.Caller.getPrivateMessage(fromUsername, errorMsg, toUserName);
             }
         }
 
--- GameSquad/src/GameSquad/wwwroot/ngApp/controllers/chatController.ts
+++ GameSquad/src/GameSquad/wwwroot/ngApp/controllers/chatController.ts
@@ -1,40 +1,304 @@
-﻿//namespace GameSquad {
+﻿namespace GameSquad.Controllers {
 
-//    declare var $;
+    declare var $;
 
-//    export class ChatController {
-//        public messages;
-//        public newMessage;
-//        private chatHub: any
+    export class ChatController {
+        //SignalR Global Messaging
+        private chatHub: any
+        public messages = [];
+        public newMessage;
+        public dupUser = false;
+
+        //total connected users
+        public connectedUsers = [
+            {
+                Username: "Dummy User 1",
+                ConnectionId: "none"
+            },
+            {
+                Username: "Dummy User 2",
+                ConnectionId: "none"
 
 
-//        constructor(public $scope: ng.IScope) {
-//            this.chatHub = $.connection.chatHub;
+            }
+        ];
 
-//        }
+        //private messaging fields
+        public sendToUser;
+        public openPrivateChat;
+        public newPrivateMessage;
+        public privateMessageArray = [
+            
+        ];
+        public privateMessagesDispalyed;
+        public newMessageAlertUser;
 
-//        public sendMessage() {
-//            console.log("Message sent to server");
-//            this.chatHub.server.sendMessage(this.newMessage);
-//            this.chatHub.newMessage = "";
-//            this.newMessage = "";
-//        }
+        public isLoggedIn() {
+            return this.accountService.isLoggedIn();
 
-//        public waitForMessages() {
-//            let messagelist = this.messages;
-//            let scope = this.$scope;
+        }
 
-//            this.chatHub.client.newMessage = function onNewMessage(message) {
-//                console.log("Message Recieved from server!");
+        public getUserName() {
+            return this.accountService.getUserName();
+        }
+
+        public setUserToMessage(user) {
+            this.sendToUser = user;
+            let index = this.privateMessageArray.map((x) => { return x.username }).indexOf(this.sendToUser.Username);
+
+            if (index > -1) {
+                //privateMessageArray.splice(index, 1);
+
+                this.privateMessagesDispalyed = this.privateMessageArray[index].messages;
+            }
+            else {
+                let newConversation = {
+                    username: this.sendToUser.Username,
+                    connectionId: this.sendToUser.ConnectionId,
+                    messages: [],
+                    newMessageAlert: false
+
+                }
+                this.privateMessageArray.push(newConversation);
+                let index = this.privateMessageArray.map((x) => { return x.username }).indexOf(this.sendToUser.Username);
+                this.privateMessagesDispalyed = this.privateMessageArray[index].messages;
+            }
+            this.openPrivateChat = true;
+            this.$scope.$apply;
+
+
+
+        }
+
+        //TODO: Merge the connected user and private message arrays so don't have to do as many array maps
+
+        //Ignore this: ng-class="{'list-group-item-success': chat.newMessageAlertUser == user.Username,'list-group-item-info': chat.sendToUser.Username == user.Username}"
+
+        public userListClass(username) {
+            let index = this.privateMessageArray.map((x) => { return x.username }).indexOf(username);
+            let conversation = this.privateMessageArray[index];
+
+            console.log(conversation);
+            console.log(this.sendToUser.Username);
+
+            if (conversation.newMessageAlert != null) {
+
+                if (conversation.username === this.sendToUser.Username) {
+                    conversation.newMessageAlert = false;
+                    return 'list-group-item-info'
+                }
                 
-//                messagelist.push({ message: message });
-//                console.log(messagelist);
-//                scope.$apply();
+                else if (conversation.newMessageAlert) {
+                    return 'list-group-item-success'
+                }
+            }
+            
+            else {
+                return ""
+            }
 
-//            };
+        }
 
-//        }
-//    }
+        //sends private message
+        public sendPrivateMessage() {
+            let fromUsername = this.accountService.getUserName();
 
-//    angular.module('GameSquad').controller('ChatController', ChatController);
-//}
+            //just in case check. Sometimes has issue
+            let index = this.connectedUsers.map((x) => { return x.Username }).indexOf(this.sendToUser.Username);
+            let userToSend = this.connectedUsers[index];
+
+            this.chatHub.server.sendPrivateMessage(fromUsername, this.newPrivateMessage, userToSend.Username, userToSend.ConnectionId);
+            this.newPrivateMessage = "";
+
+        }
+
+        //sends global message
+        public sendMessage() {
+            let username = this.accountService.getUserName();
+            let messageToSend = { username: username, message: this.newMessage }
+            console.log(messageToSend);
+            this.chatHub.server.sendMessage(messageToSend);
+            console.log("Message sent to server");
+
+            this.newMessage = "";
+        }
+
+        //Waits for messages from server
+        public waitForMessages() {
+            let messagelist = this.messages;
+            let scope = this.$scope;
+            let privateMessageArray = this.privateMessageArray;
+            let messageAlert;
+
+            this.chatHub.client.newMessage = function onNewMessage(messageRecieved) {
+                console.log("Message Recieved from server!");
+
+                messagelist.push(messageRecieved);
+                console.log(messagelist);
+                scope.$apply();
+
+            };
+
+            this.chatHub.client.getPrivateMessage = function onNewMessage(fromUsername, privateMessage, conversationName) {
+                console.log("Private message Recieved from server!");
+
+
+
+                let newMessage = { username: fromUsername, message: privateMessage };
+
+                let index = privateMessageArray.map((x) => { return x.username }).indexOf(conversationName);
+                console.log(index);
+                if (index > -1) {
+
+
+                    let chosenUser = privateMessageArray[index];
+
+                    chosenUser.messages.push(newMessage);
+                    chosenUser.newMessageAlert = true;
+                }
+                else {
+                    let newConversation = {
+                        username: conversationName,
+                        messages: [newMessage],
+                        newMessageAlert: true
+
+                    }
+                    privateMessageArray.push(newConversation);
+                }
+                //console.log(messageAlert);
+
+                //messageAlert = conversationName;
+                //console.log(messageAlert);
+               
+                scope.$apply();
+
+
+            };
+           // this.newMessageAlertUser = messageAlert;
+
+        }
+
+
+
+        public getConnectedUsers() {
+            let connectedUsers = this.connectedUsers;
+            let scope = this.$scope;
+            let dupUser = this.dupUser;
+
+            this.chatHub.client.onConnected = function onConnected(userList) {
+                console.log("Userlist Recieved from server!");
+
+                console.log(userList + typeof userList);
+
+                if (userList === 0) {
+                    $.connection.hub.stop();
+                    dupUser = true;
+                }
+
+                else {
+                    for (var user of userList) {
+                        connectedUsers.push(user);
+                        console.log(user.Username);
+                    }
+                    console.log(connectedUsers);
+                }
+                scope.$apply();
+
+
+            }
+
+            this.chatHub.client.onNewUserConnected = function onNewUserConnected(newUser) {
+                console.log("New user Recieved from server!");
+
+                connectedUsers.push(newUser);
+                console.log(connectedUsers);
+
+                scope.$apply();
+
+            }
+            this.chatHub.client.onUserDisconnected = function onUserDisconnected(userToRemove) {
+                console.log("User has disconnected " + userToRemove);
+
+
+                let index = connectedUsers.map((x) => { return x.Username }).indexOf(userToRemove);
+                console.log(index);
+                if (index > -1) {
+                    connectedUsers.splice(index, 1);
+                    scope.$apply();
+                }
+
+            }
+
+        }
+
+        public signalrStart() {
+
+            let userName = this.accountService.getUserName();
+
+
+            if (this.accountService.isLoggedIn()) {
+                $.connection.hub.start().done(function () {
+                    //sends username to Server
+                    $.connection.chatHub.server.connect(userName);
+                });
+                $.connection.hub.error(function (err) {
+                    console.log("An error occurded: " + err);
+                });
+
+                //signalr waiting methods
+                this.waitForMessages();
+                this.getConnectedUsers();
+            }
+            
+        }
+
+        public signalrWait() {
+
+        }
+
+        constructor(private accountService: GameSquad.Services.AccountService,
+            private $scope: ng.IScope
+        ) {
+
+            console.log("Chat Constructor Running!");
+            this.chatHub = $.connection.chatHub;
+            $.connection.hub.logging = true;
+
+            
+
+            setTimeout(function () {
+                console.log("hi!");
+                if (accountService.isLoggedIn()) {
+                    let userName = accountService.getUserName();
+
+                    $.connection.hub.start().done(function () {
+                        //sends username to Server
+                        $.connection.chatHub.server.connect(userName);
+                    });
+                    $.connection.hub.error(function (err) {
+                        console.log("An error occurded: " + err);
+                    });
+
+                    //signalr waiting methods
+                    //this.waitForMessages();
+                    //this.getConnectedUsers();
+                }
+            }, 2000);
+
+            this.waitForMessages();
+            this.getConnectedUsers();
+            
+            
+            //function signalrWait() {
+            //    setTimeout(function () {
+                   
+            //    }, 2000);
+            //} ();
+
+        }
+
+       
+    }
+
+    angular.module('GameSquad').controller('ChatController', ChatController);
+}
